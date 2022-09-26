bb06d6261684ca4e0f1603675863222af6070f70
Zuhrizal <zuhrizalmuhammad.xrpl@gmail.com>
2022-09-26 20:47:49 +0700

    Make Sorting Product And Showing Product Per Page

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -11,6 +11,15 @@ class ShopComponent extends Component
 {
     use WithPagination;
 
+    public $sorting;
+    public $pagesize;
+
+    public function mount()
+    {
+        $this->sorting = 'default';
+        $this->pagesize = 12;
+    }
+
     public function store($product_id, $product_name, $product_price)
     {
         Cart::add($product_id, $product_name, 1, $product_price)->associate('App\Models\Product');
@@ -20,7 +29,22 @@ class ShopComponent extends Component
 
     public function render()
     {
-        $products = Product::paginate(12);
+        if ($this->sorting == 'date')
+        {
+            $products = Product::orderBy('created_at', 'DESC')->paginate($this->pagesize);
+        }
+        else if ($this->sorting == 'price')
+        {
+            $products = Product::orderBy('regular_price', 'ASC')->paginate($this->pagesize);
+        }
+        else if ($this->sorting == 'price_desc')
+        {
+            $products = Product::orderBy('regular_price', 'DESC')->paginate($this->pagesize);
+        }
+        else
+        {
+            $products = Product::paginate($this->pagesize);
+        }
         return view('livewire.shop-component', ['products' => $products])->layout('layouts.base');
     }
 }
