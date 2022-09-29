93606b175ffc450fc77bd4bb7ea69ecac0aad441
Zuhrizal <zuhrizalmuhammad.xrpl@gmail.com>
2022-09-29 09:28:53 +0700

    Give Categories Access To Admin

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Livewire\Admin\AdminCategoryComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\CategoryComponent;
@@ -54,4 +55,5 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
 // For Admin
 Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function () {
     Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
+    Route::get('/admin/categories', AdminCategoryComponent::class)->name('admin.categories');
 });
