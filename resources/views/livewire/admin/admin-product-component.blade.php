38ee41b16c7d5e9a94f2e4a6e8da835edf85e126
Zuhrizal <zuhrizalmuhammad.xrpl@gmail.com>
2022-10-02 13:33:40 +0700

    Add New Product For Admin

--- resources/views/livewire/admin/admin-product-component.blade.php
+++ resources/views/livewire/admin/admin-product-component.blade.php
@@ -7,12 +7,19 @@
             display: block !important;
         }
     </style>
-    <div class="container">
+    <div class="container" style="padding: 30px 0">
         <div class="row">
             <div class="col-md-12">
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        All Products
+                        <div class="row">
+                            <div class="col-md-6">
+                                All Products
+                            </div>
+                            <div class="col-md-6">
+                                <a href="{{ route('admin.addproduct') }}" class="btn btn-success pull-right">Add New</a>
+                            </div>
+                        </div>
                     </div>
                     <div class="panel-body">
                         <table class="table table-striped">
