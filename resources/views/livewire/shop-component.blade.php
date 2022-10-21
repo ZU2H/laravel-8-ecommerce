2ab406f69453856fc1d7a70909044f99ff58f108
Zuhrizal <zuhrizalmuhammad.xrpl@gmail.com>
2022-10-21 20:12:42 +0700

    Add Price Filter On Shop Page

--- resources/views/livewire/shop-component.blade.php
+++ resources/views/livewire/shop-component.blade.php
@@ -62,15 +62,20 @@
                             <li class="col-lg-4 col-md-6 col-sm-6 col-xs-6 ">
                                 <div class="product product-style-3 equal-elem ">
                                     <div class="product-thumnail">
-                                        <a href="{{ route('product.details', ['slug' => $product->slug]) }}" title="{{$product->name}}">
-                                            <figure><img src="{{ asset('assets/images/products/'.$product->image) }}"
-                                                    alt="{{$product->name}}"></figure>
+                                        <a href="{{ route('product.details', ['slug' => $product->slug]) }}"
+                                            title="{{ $product->name }}">
+                                            <figure><img src="{{ asset('assets/images/products/' . $product->image) }}"
+                                                    alt="{{ $product->name }}"></figure>
                                         </a>
                                     </div>
                                     <div class="product-info">
-                                        <a href="{{ route('product.details', ['slug' => $product->slug]) }}" class="product-name"><span>{{$product->name}}</span></a>
-                                        <div class="wrap-price"><span class="product-price">${{$product->regular_price}}</span></div>
-                                        <a href="#" class="btn add-to-cart" wire:click.prevent="store({{$product->id}}, '{{$product->name}}', {{$product->regular_price}})"">Add To Cart</a>
+                                        <a href="{{ route('product.details', ['slug' => $product->slug]) }}"
+                                            class="product-name"><span>{{ $product->name }}</span></a>
+                                        <div class="wrap-price"><span
+                                                class="product-price">${{ $product->regular_price }}</span></div>
+                                        <a href="#" class="btn add-to-cart"
+                                            wire:click.prevent="store({{ $product->id }}, '{{ $product->name }}', {{ $product->regular_price }})"">Add
+                                            To Cart</a>
                                     </div>
                                 </div>
                             </li>
@@ -80,7 +85,7 @@
                 </div>
 
                 <div class="wrap-pagination-info">
-                    {{$products->links()}}
+                    {{ $products->links() }}
                     {{-- <ul class="page-numbers">
                         <li><span class="page-number-item current">1</span></li>
                         <li><a class="page-number-item" href="#">2</a></li>
@@ -98,9 +103,10 @@
                     <div class="widget-content">
                         <ul class="list-category">
                             @foreach ($categories as $category)
-                            <li class="category-item">
-                                <a href="{{ route('product.category', ['category_slug' => $category->slug]) }}" class="cate-link">{{$category->name}}</a>
-                            </li>
+                                <li class="category-item">
+                                    <a href="{{ route('product.category', ['category_slug' => $category->slug]) }}"
+                                        class="cate-link">{{ $category->name }}</a>
+                                </li>
                             @endforeach
                         </ul>
                     </div>
@@ -135,14 +141,9 @@
                 </div><!-- brand widget-->
 
                 <div class="widget mercado-widget filter-widget price-filter">
-                    <h2 class="widget-title">Price</h2>
+                    <h2 class="widget-title">Price <span class="text-info">${{$min_price}} - ${{$max_price}}</span></h2>
                     <div class="widget-content">
-                        <div id="slider-range"></div>
-                        <p>
-                            <label for="amount">Price:</label>
-                            <input type="text" id="amount" readonly>
-                            <button class="filter-submit">Filter</button>
-                        </p>
+                        <div id="slider" wire:ignore></div>
                     </div>
                 </div><!-- Price-->
 
@@ -272,3 +273,27 @@
     <!--end container-->
 
 </main>
+
+@push('scripts')
+    <script>
+        var slider = document.getElementById('slider');
+        noUiSlider.create(slider, {
+            start: [1, 1000],
+            connect: true,
+            range: {
+                'min': 1,
+                'max': 1000
+            },
+            pips: {
+                mode: 'steps',
+                stepped: true,
+                dencity: 4
+            }
+        });
+
+        slider.noUiSlider.on('update', function(value) {
+            @this.set('min_price', value[0]);
+            @this.set('max_price', value[1]);
+        });
+    </script>
+@endpush
