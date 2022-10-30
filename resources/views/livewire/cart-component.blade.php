d6b1cf8afef67f3668899531240335ecc13f0580
Zuhrizal <zuhrizalmuhammad.xrpl@gmail.com>
2022-10-30 22:21:17 +0700

    Add SaveForLater Option on Cart Page

--- resources/views/livewire/cart-component.blade.php
+++ resources/views/livewire/cart-component.blade.php
@@ -13,64 +13,73 @@
 
                 @if (Session::has('success_message'))
                     <div class="alert alert-success">
-                        <strong>Success</strong> {{Session::get('success_message')}}
+                        <strong>Success</strong> {{ Session::get('success_message') }}
                     </div>
                 @endif
 
                 @if (Cart::instance('cart')->count() > 0)
 
-                <h3 class="box-title">Products Name</h3>
-                <ul class="products-cart">
+                    <h3 class="box-title">Products Name</h3>
+                    <ul class="products-cart">
 
-                    @foreach (Cart::instance('cart')->content() as $item)
+                        @foreach (Cart::instance('cart')->content() as $item)
+                            <li class="pr-cart-item">
+                                <div class="product-image">
+                                    <figure><img src="{{ asset('assets/images/products/' . $item->model->image) }}"
+                                            alt="{{ $item->model->name }}"></figure>
+                                </div>
+                                <div class="product-name">
+                                    <a class="link-to-product"
+                                        href="{{ route('product.details', ['slug' => $item->model->slug]) }}">{{ $item->model->name }}</a>
+                                </div>
+                                <div class="price-field produtc-price">
+                                    <p class="price">${{ $item->model->regular_price }}</p>
+                                </div>
+                                <div class="quantity">
+                                    <div class="quantity-input">
+                                        <input type="text" name="product-quatity" value="{{ $item->qty }}"
+                                            data-max="120" pattern="[0-9]*">
+                                        <a class="btn btn-increase" href="#"
+                                            wire:click.prevent="increaseQuantity('{{ $item->rowId }}')"></a>
+                                        <a class="btn btn-reduce" href="#"
+                                            wire:click.prevent="decreaseQuantity('{{ $item->rowId }}')"></a>
+                                    </div>
+                                    <p class="text-center">
+                                        <a href="#"
+                                            wire:click.prevent="switchToSaveForLater('{{ $item->rowId }}')">
+                                            Save ForLater
+                                        </a>
+                                    </p>
+                                </div>
+                                <div class="price-field sub-total">
+                                    <p class="price">${{ $item->subtotal }}</p>
+                                </div>
+                                <div class="delete">
+                                    <a href="#" wire:click.prevent="destroy('{{ $item->rowId }}')"
+                                        class="btn btn-delete" title="">
+                                        <span>Delete from your cart</span>
+                                        <i class="fa fa-times-circle" aria-hidden="true"></i>
+                                    </a>
+                                </div>
+                            </li>
+                        @endforeach
 
-                    <li class="pr-cart-item">
-                        <div class="product-image">
-                            <figure><img src="{{ asset('assets/images/products/'.$item->model->image) }}" alt="{{$item->model->name}}"></figure>
-                        </div>
-                        <div class="product-name">
-                            <a class="link-to-product" href="{{ route('product.details', ['slug' => $item->model->slug]) }}">{{$item->model->name}}</a>
-                        </div>
-                        <div class="price-field produtc-price">
-                            <p class="price">${{$item->model->regular_price}}</p>
-                        </div>
-                        <div class="quantity">
-                            <div class="quantity-input">
-                                <input type="text" name="product-quatity" value="{{$item->qty}}" data-max="120"
-                                    pattern="[0-9]*">
-                                <a class="btn btn-increase" href="#" wire:click.prevent="increaseQuantity('{{$item->rowId}}')"></a>
-                                <a class="btn btn-reduce" href="#" wire:click.prevent="decreaseQuantity('{{$item->rowId}}')"></a>
-                            </div>
-                        </div>
-                        <div class="price-field sub-total">
-                            <p class="price">${{$item->subtotal}}</p>
-                        </div>
-                        <div class="delete">
-                            <a href="#" wire:click.prevent="destroy('{{$item->rowId}}')" class="btn btn-delete" title="">
-                                <span>Delete from your cart</span>
-                                <i class="fa fa-times-circle" aria-hidden="true"></i>
-                            </a>
-                        </div>
-                    </li>
-
-                    @endforeach
-
-                </ul>
-
+                    </ul>
                 @else
-
-                <p>No item in cart</p>
-
+                    <p>No item in cart</p>
                 @endif
             </div>
 
             <div class="summary">
                 <div class="order-summary">
                     <h4 class="title-box">Order Summary</h4>
-                    <p class="summary-info"><span class="title">Subtotal</span><b class="index">${{Cart::instance('cart')->subtotal()}}</b></p>
-                    <p class="summary-info"><span class="title">Tax</span><b class="index">${{Cart::instance('cart')->tax()}}</b></p>
+                    <p class="summary-info"><span class="title">Subtotal</span><b
+                            class="index">${{ Cart::instance('cart')->subtotal() }}</b></p>
+                    <p class="summary-info"><span class="title">Tax</span><b
+                            class="index">${{ Cart::instance('cart')->tax() }}</b></p>
                     <p class="summary-info"><span class="title">Shipping</span><b class="index">Free Shipping</b></p>
-                    <p class="summary-info total-info "><span class="title">Total</span><b class="index">${{Cart::instance('cart')->total()}}</b>
+                    <p class="summary-info total-info "><span class="title">Total</span><b
+                            class="index">${{ Cart::instance('cart')->total() }}</b>
                     </p>
                 </div>
                 <div class="checkout-info">
@@ -88,6 +97,57 @@
                 </div>
             </div>
 
+            <div class="wrap-iten-in-cart">
+                <h3 class="title-box" style="border-bottom: 1px solid; padding-bottom: 15px;">
+                    {{ Cart::instance('saveForLater')->count() }} item(s) Save For Later</h3>
+                @if (Session::has('s_success_message'))
+                    <div class="alert alert-success">
+                        <strong>Success</strong> {{ Session::get('s_success_message') }}
+                    </div>
+                @endif
+
+                @if (Cart::instance('saveForLater')->count() > 0)
+
+                    <h3 class="box-title">Products Name</h3>
+                    <ul class="products-cart">
+
+                        @foreach (Cart::instance('saveForLater')->content() as $item)
+                            <li class="pr-cart-item">
+                                <div class="product-image">
+                                    <figure><img src="{{ asset('assets/images/products/' . $item->model->image) }}"
+                                            alt="{{ $item->model->name }}"></figure>
+                                </div>
+                                <div class="product-name">
+                                    <a class="link-to-product"
+                                        href="{{ route('product.details', ['slug' => $item->model->slug]) }}">{{ $item->model->name }}</a>
+                                </div>
+                                <div class="price-field produtc-price">
+                                    <p class="price">${{ $item->model->regular_price }}</p>
+                                </div>
+                                <div class="quantity">
+                                    <p class="text-center">
+                                        <a href="#"
+                                            wire:click.prevent="moveToCart('{{ $item->rowId }}')">
+                                            Move To Cart
+                                        </a>
+                                    </p>
+                                </div>
+                                <div class="delete">
+                                    <a href="#" wire:click.prevent="deleteFromSaveForLater('{{ $item->rowId }}')"
+                                        class="btn btn-delete" title="">
+                                        <span>Delete from save for later</span>
+                                        <i class="fa fa-times-circle" aria-hidden="true"></i>
+                                    </a>
+                                </div>
+                            </li>
+                        @endforeach
+
+                    </ul>
+                @else
+                    <p>No item in saved for later</p>
+                @endif
+            </div>
+
             <div class="wrap-show-advance-info-box style-1 box-in-site">
                 <h3 class="title-box">Most Viewed Products</h3>
                 <div class="wrap-products">
@@ -98,8 +158,9 @@
                         <div class="product product-style-2 equal-elem ">
                             <div class="product-thumnail">
                                 <a href="#" title="T-Shirt Raw Hem Organic Boro Constrast Denim">
-                                    <figure><img src="{{ asset('assets/images/products/digital_04.jpg') }}" width="214"
-                                            height="214" alt="T-Shirt Raw Hem Organic Boro Constrast Denim">
+                                    <figure><img src="{{ asset('assets/images/products/digital_04.jpg') }}"
+                                            width="214" height="214"
+                                            alt="T-Shirt Raw Hem Organic Boro Constrast Denim">
                                     </figure>
                                 </a>
                                 <div class="group-flash">
@@ -119,8 +180,9 @@
                         <div class="product product-style-2 equal-elem ">
                             <div class="product-thumnail">
                                 <a href="#" title="T-Shirt Raw Hem Organic Boro Constrast Denim">
-                                    <figure><img src="{{ asset('assets/images/products/digital_17.jpg') }}" width="214"
-                                            height="214" alt="T-Shirt Raw Hem Organic Boro Constrast Denim">
+                                    <figure><img src="{{ asset('assets/images/products/digital_17.jpg') }}"
+                                            width="214" height="214"
+                                            alt="T-Shirt Raw Hem Organic Boro Constrast Denim">
                                     </figure>
                                 </a>
                                 <div class="group-flash">
@@ -144,8 +206,9 @@
                         <div class="product product-style-2 equal-elem ">
                             <div class="product-thumnail">
                                 <a href="#" title="T-Shirt Raw Hem Organic Boro Constrast Denim">
-                                    <figure><img src="{{ asset('assets/images/products/digital_15.jpg') }}" width="214"
-                                            height="214" alt="T-Shirt Raw Hem Organic Boro Constrast Denim">
+                                    <figure><img src="{{ asset('assets/images/products/digital_15.jpg') }}"
+                                            width="214" height="214"
+                                            alt="T-Shirt Raw Hem Organic Boro Constrast Denim">
                                     </figure>
                                 </a>
                                 <div class="group-flash">
@@ -170,8 +233,9 @@
                         <div class="product product-style-2 equal-elem ">
                             <div class="product-thumnail">
                                 <a href="#" title="T-Shirt Raw Hem Organic Boro Constrast Denim">
-                                    <figure><img src="{{ asset('assets/images/products/digital_01.jpg') }}" width="214"
-                                            height="214" alt="T-Shirt Raw Hem Organic Boro Constrast Denim">
+                                    <figure><img src="{{ asset('assets/images/products/digital_01.jpg') }}"
+                                            width="214" height="214"
+                                            alt="T-Shirt Raw Hem Organic Boro Constrast Denim">
                                     </figure>
                                 </a>
                                 <div class="group-flash">
@@ -191,8 +255,9 @@
                         <div class="product product-style-2 equal-elem ">
                             <div class="product-thumnail">
                                 <a href="#" title="T-Shirt Raw Hem Organic Boro Constrast Denim">
-                                    <figure><img src="{{ asset('assets/images/products/digital_21.jpg') }}" width="214"
-                                            height="214" alt="T-Shirt Raw Hem Organic Boro Constrast Denim">
+                                    <figure><img src="{{ asset('assets/images/products/digital_21.jpg') }}"
+                                            width="214" height="214"
+                                            alt="T-Shirt Raw Hem Organic Boro Constrast Denim">
                                     </figure>
                                 </a>
                                 <div class="wrap-btn">
@@ -209,8 +274,9 @@
                         <div class="product product-style-2 equal-elem ">
                             <div class="product-thumnail">
                                 <a href="#" title="T-Shirt Raw Hem Organic Boro Constrast Denim">
-                                    <figure><img src="{{ asset('assets/images/products/digital_03.jpg') }}" width="214"
-                                            height="214" alt="T-Shirt Raw Hem Organic Boro Constrast Denim">
+                                    <figure><img src="{{ asset('assets/images/products/digital_03.jpg') }}"
+                                            width="214" height="214"
+                                            alt="T-Shirt Raw Hem Organic Boro Constrast Denim">
                                     </figure>
                                 </a>
                                 <div class="group-flash">
@@ -234,8 +300,9 @@
                         <div class="product product-style-2 equal-elem ">
                             <div class="product-thumnail">
                                 <a href="#" title="T-Shirt Raw Hem Organic Boro Constrast Denim">
-                                    <figure><img src="{{ asset('assets/images/products/digital_04.jpg') }}" width="214"
-                                            height="214" alt="T-Shirt Raw Hem Organic Boro Constrast Denim">
+                                    <figure><img src="{{ asset('assets/images/products/digital_04.jpg') }}"
+                                            width="214" height="214"
+                                            alt="T-Shirt Raw Hem Organic Boro Constrast Denim">
                                     </figure>
                                 </a>
                                 <div class="group-flash">
@@ -255,8 +322,9 @@
                         <div class="product product-style-2 equal-elem ">
                             <div class="product-thumnail">
                                 <a href="#" title="T-Shirt Raw Hem Organic Boro Constrast Denim">
-                                    <figure><img src="{{ asset('assets/images/products/digital_05.jpg') }}" width="214"
-                                            height="214" alt="T-Shirt Raw Hem Organic Boro Constrast Denim">
+                                    <figure><img src="{{ asset('assets/images/products/digital_05.jpg') }}"
+                                            width="214" height="214"
+                                            alt="T-Shirt Raw Hem Organic Boro Constrast Denim">
                                     </figure>
                                 </a>
                                 <div class="group-flash">
