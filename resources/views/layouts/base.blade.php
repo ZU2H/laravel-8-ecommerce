122f62b246519106fd34f628d58c41226a8dcf60
Zuhrizal <zuhrizalmuhammad.xrpl@gmail.com>
2022-11-06 16:57:20 +0700

    Admin Show Orders

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -142,6 +142,9 @@
                                                         <a title="All Coupons" href="{{ route('admin.coupons') }}">All Coupons</a>
                                                     </li>
                                                     <li class="menu-item">
+                                                        <a title="All Orders" href="{{ route('admin.orders') }}">All Orders</a>
+                                                    </li>
+                                                    <li class="menu-item">
                                                         <a href="{{ route('logout') }}"
                                                             onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
                                                     </li>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use App\Http\Livewire\Admin\AdminEditHomeSliderComponent;
 use App\Http\Livewire\Admin\AdminEditProductComponent;
 use App\Http\Livewire\Admin\AdminHomeCategoryComponent;
 use App\Http\Livewire\Admin\AdminHomeSliderComponent;
+use App\Http\Livewire\Admin\AdminOrderComponent;
 use App\Http\Livewire\Admin\AdminProductComponent;
 use App\Http\Livewire\Admin\AdminSaleComponent;
 use App\Http\Livewire\CartComponent;
@@ -94,4 +95,6 @@ Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function () 
     Route::get('/admin/coupons', AdminCouponsComponent::class)->name('admin.coupons');
     Route::get('/admin/coupons/add', AdminAddCouponsComponent::class)->name('admin.addcoupon');
     Route::get('/admin/coupons/edit/{coupon_id}', AdminEditCouponsComponent::class)->name('admin.editcoupon');
+
+    Route::get('/admin/orders', AdminOrderComponent::class)->name('admin.orders');
 });
