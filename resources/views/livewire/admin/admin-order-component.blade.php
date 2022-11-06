9d6cb2d0312192059568de2b3cc1d7b210bd1ffd
Zuhrizal <zuhrizalmuhammad.xrpl@gmail.com>
2022-11-06 21:58:44 +0700

    Admin Update Order Status

--- resources/views/livewire/admin/admin-order-component.blade.php
+++ resources/views/livewire/admin/admin-order-component.blade.php
@@ -19,8 +19,8 @@
                         </div>
                     </div>
                     <div class="panel-body">
-                        @if (Session::has('message'))
-                            <div class="alert alert-success" role="alert">{{ Session::get('message') }}</div>
+                        @if (Session::has('order_message'))
+                            <div class="alert alert-success" role="alert">{{ Session::get('order_message') }}</div>
                         @endif
                         <table class="table table-striped">
                             <thead>
@@ -37,7 +37,7 @@
                                     <th>Zipcode</th>
                                     <th>Status</th>
                                     <th>Order Date</th>
-                                    <th>Action</th>
+                                    <th colspan="2" class="text-center">Action</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -58,6 +58,17 @@
                                         <td>
                                             <a href="{{ route('admin.orderdetails', ['order_id' => $order->id]) }}"
                                                 class="btn btn-info btn-sm">Details</a>
+                                        </td>
+                                        <td>
+                                            <div class="dropdown">
+                                                <button class="btn btn-success btn-sm dropdown-toggle" type="button" data-toggle="dropdown">Status
+                                                    <span class="caret"></span>
+                                                </button>
+                                                <ul class="dropdown-menu">
+                                                    <li><a href="#" wire:click.prevent="updateOrderStatus({{$order->id}}, 'delivered')">Delivered</a></li>
+                                                    <li><a href="#" wire:click.prevent="updateOrderStatus({{$order->id}}, 'canceled')">Canceled</a></li>
+                                                </ul>
+                                            </div>
                                         </td>
                                     </tr>
                                 @endforeach
