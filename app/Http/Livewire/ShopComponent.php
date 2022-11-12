6abb7db6c086d6dd5c57bcadaeebb6c14a721ce9
Zuhrizal <zuhrizalmuhammad.xrpl@gmail.com>
2022-11-12 17:12:28 +0700

    Shopping Cart Using Database

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -7,6 +7,7 @@ use App\Models\Product;
 use Livewire\Component;
 use Livewire\WithPagination;
 use Cart;
+use Illuminate\Support\Facades\Auth;
 
 class ShopComponent extends Component
 {
@@ -73,6 +74,12 @@ class ShopComponent extends Component
         }
 
         $categories = Category::all();
+
+        if (Auth::check())
+        {
+            Cart::instance('cart')->store(Auth::user()->email);
+        }
+
         return view('livewire.shop-component', [
             'products' => $products,
             'categories' => $categories
