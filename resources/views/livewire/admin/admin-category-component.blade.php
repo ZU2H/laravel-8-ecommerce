24cbd946770e9ec88d5f1fa0c700edf0eeee0388
Zuhrizal <zuhrizalmuhammad.xrpl@gmail.com>
2022-11-12 17:55:16 +0700

    Admin Show Subcategories With Category

--- resources/views/livewire/admin/admin-category-component.blade.php
+++ resources/views/livewire/admin/admin-category-component.blade.php
@@ -8,6 +8,10 @@
         nav .hidden {
             display: block !important;
         }
+
+        .sclist {
+            list-style: none;
+        }
     </style>
     <div class="container" style="padding: 30px 0;">
         <div class="row">
@@ -31,6 +35,7 @@
                                     <th>Id</th>
                                     <th>Category Name</th>
                                     <th>Slug</th>
+                                    <th>Sub Category</th>
                                     <th>Action</th>
                                 </tr>
                             </thead>
@@ -40,6 +45,13 @@
                                         <td>{{ $category->id }}</td>
                                         <td>{{ $category->name }}</td>
                                         <td>{{ $category->slug }}</td>
+                                        <td>
+                                            <ul class="sclist">
+                                                @foreach ($category->subCategories as $scategory)
+                                                    <li><i class="fa fa-caret-right"></i> {{$scategory->name}}</li>
+                                                @endforeach
+                                            </ul>
+                                        </td>
                                         <td>
                                             <a href="{{ route('admin.editcategory', ['category_slug' => $category->slug]) }}"><i
                                                     class="fa fa-edit fa-2x"></i></a>
